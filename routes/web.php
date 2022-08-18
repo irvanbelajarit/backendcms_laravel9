1b077d2262e52a4ebdc7b5b303aee6058d558638
irvanln <irvina22@gmail.com>
2022-08-18 15:13:35 +0700

    routes update

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-// Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard.index');
+Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard.index');
