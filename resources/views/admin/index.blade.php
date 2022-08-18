ba204fc2821df9e7b12aa1a0fb672e92de788e3c
irvanln <irvina22@gmail.com>
2022-08-18 15:31:45 +0700

    tambah fungsi logout

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.dashboard')
 
 @section('title')
-
+admin dashboard
 @endsection
 
 @section('breadcrumbs')
-
+breadcrumb
 @endsection
 
 @section('content')
     <h1>dashboard</h1>
+    @yield('breadcrumbs')
 @endsection
